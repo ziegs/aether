5c0ff424a9aee284551004a54d062207030c8580
Matthew Ziegelbaum <mziegelbaum@gmail.com>
2009-12-02 14:55:01 -0500

    make js file, start doing real dev work on the frontend

---
+++ static/javascript/aether.js
@@ -0,0 +1,11 @@
+/**
+ * @author Matthew Ziegelbaum <mziegelbaum@acm.jhu.edu>
+ * @fileoverview Implements most of the functionality for aether.
+ */
+
+/**
+ * Initializes Google Map and basic overlays.
+ */
+function load() {
+  
+};
