ef7c33c82c739e93a56eec8b0a2bf9b325f3bb08
Matthew Ziegelbaum <mziegelbaum@gmail.com>
2009-12-02 01:41:46 -0500

    add minimal compass/sass/haml stuff

---
+++ aether.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+require 'sass'
+require 'compass'
+require 'mysql'
+
+configure do
+  Compass.configuration.parse(File.join(Sinatra::Application.root, 'config', 'compass.config'))
+
+  set :haml, { :format => :html5 }
+  set :sass, Compass.sass_engine_options
+end
+
+get '/stylesheets/:name.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass(:"stylesheets/#{params[:name]}", Compass.sass_engine_options )
+end
+
+get '/' do
+  haml :index
+end
