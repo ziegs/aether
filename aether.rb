f48611e87ef35d492855aa782dec194571316720
Matthew Ziegelbaum <mziegelbaum@gmail.com>
2009-12-03 09:48:24 -0500

    make the queries hash ruby-style and use less lines

--- aether.rb
+++ aether.rb
@@ -6,28 +6,21 @@ require 'sass'
 require 'compass'
 require 'mysql'
 
-queries = {}
-queries['1'] = 'AllAirports'
-queries['2'] = 'AllAirlines'
-queries['3'] = 'AllRoutes'
-#queries['4'] = 'AllAirlines'
-queries['5'] = 'AirportsAirlineServices'
-#queries['6'] = 'AllAirlines'
-queries['7'] = 'RoutesAirlineServices'
-#queries['8'] = 'AllAirlines'
-queries['9a'] = 'AirlinesLeavingAirport'
-queries['9b'] = 'AirlinesEnteringAirport'
-queries['10'] = 'DestinationsFromAirport'
-queries['11'] = 'AirportDistance'
-queries['12'] = 'AirportTimeDifference'
-#queries['13'] = 'AllAirlines'
-#queries['14'] = 'AllAirlines'
-#queries['15'] = 'AllAirlines'
-queries['16'] = 'CostBetweenAirports'
-#queries['17'] = 'AllAirlines'
-#queries['18'] = 'AllAirlines'
-queries['19'] = 'AirportAtMaxElevation'
-queries['20'] = 'AirportAtMinElevation'
+queries = {
+  '1' => 'AllAirports',
+  '2' => 'AllAirlines',
+  '3' => 'AllRoutes',
+  '5' => 'AirportsAirlineServices',
+  '7' => 'RoutesAirlineServices',
+  '9a' => 'AirlinesLeavingAirport',
+  '9b' => 'AirlinesEnteringAirport',
+  '10' => 'DestinationsFromAirpot',
+  '11' => 'AirportDistance',
+  '12' => 'AirportTimeDifference',
+  '16' => 'CostBetweenAirports',
+  '19' => 'AirportAtMaxElevation',
+  '20' => 'AirportAtMinElevation'
+}
 
 configure do
   Compass.configuration.parse(File.join(Sinatra::Application.root, 'config', 'compass.config'))
