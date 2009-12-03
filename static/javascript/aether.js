84b2ec5146f0c3e5aa62abeb70e16bb5eefd1e41
Matthew Ziegelbaum <mziegelbaum@gmail.com>
2009-12-03 01:18:15 -0500

    fix the javascript, add a tablesorter plugin

--- static/javascript/aether.js
+++ static/javascript/aether.js
@@ -3,25 +3,30 @@
  * @fileoverview Implements most of the functionality for aether.
  */
 
+var mapObj;
+var markerManager;
+
 /**
  * Initializes Google Map and basic overlays.
  */
 function load() {
   var map = new GMap2(document.getElementById('map'));
   map.setMapType(G_HYBRID_MAP);
-  map.addControl(new GLargeMapControl());
+  map.setUIToDefault();
   map.addControl(new GOverviewMapControl());
   map.enableDoubleClickZoom();
-  map.setUIToDefault();
   
+  // For some reason, the marker manager will only work if the center is set.
+  var ftWorth = new GLatLng(32.896828000,-97.037997000);
+  map.setCenter(ftWorth, 7);
   
   var mgr = new MarkerManager(map, {
        trackMarkers: true
      });
-  var ftWorth = new GLatLng(32.896828000,-97.037997000);
-  map.setCenter(ftWorth, 7);
+  mapObj = map;
+  markerManager = mgr;
 };
 
 function unload() {
   GUnload();
-}
+};
