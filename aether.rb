d7b5823064cca2751effda26a2fd5bb41feeab89
Parker <Parker@parker.local>
2009-12-03 00:16:40 -0500

    Added query dictionary to aether.rb, added temp images for logo.

--- aether.rb
+++ aether.rb
@@ -7,8 +7,27 @@ require 'compass'
 require 'mysql'
 
 queries = {}
-queries['1'] = 'shelton'
-queries['2'] = 'zeigelbaum'
+queries['1'] = 'AllAirports'
+queries['2'] = 'AllAirlines'
+queries['3'] = 'AllRoutes'
+#queries['4'] = 'AllAirlines'
+queries['5'] = 'AirportsAirlineServices'
+#queries['6'] = 'AllAirlines'
+queries['7'] = 'RoutesAirlineServices'
+#queries['8'] = 'AllAirlines'
+queries['9a'] = 'AirlinesLeavingAirport'
+queries['9b'] = 'AirlinesEnteringAirport'
+queries['10'] = 'DestinationsFromAirport'
+queries['11'] = 'AirportDistance'
+queries['12'] = 'AirportTimeDifference'
+#queries['13'] = 'AllAirlines'
+#queries['14'] = 'AllAirlines'
+#queries['15'] = 'AllAirlines'
+queries['16'] = 'CostBetweenAirports'
+#queries['17'] = 'AllAirlines'
+#queries['18'] = 'AllAirlines'
+queries['19'] = 'AirportAtMaxElevation'
+queries['20'] = 'AirportAtMinElevation'
 
 configure do
   Compass.configuration.parse(File.join(Sinatra::Application.root, 'config', 'compass.config'))
@@ -30,5 +49,5 @@ end
 
 get '/ar' do
   content_type 'text/json'
-  print params['id']
+  print queries[params['id']]
 end
