e9263b58c5482297e80d7993d67e98ca2ee79830
Parker <Parker@parker.local>
2009-12-02 23:44:35 -0500

    Added beginner dictionary to aether.rb

--- aether.rb
+++ aether.rb
@@ -6,11 +6,16 @@ require 'sass'
 require 'compass'
 require 'mysql'
 
+queries = {}
+queries['1'] = 'shelton'
+queries['2'] = 'zeigelbaum'
+
 configure do
   Compass.configuration.parse(File.join(Sinatra::Application.root, 'config', 'compass.config'))
 
   set :haml, { :format => :html5 }
   set :sass, Compass.sass_engine_options
+  
 end
 
 get '/stylesheets/:name.css' do
